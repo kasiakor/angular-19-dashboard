704ac573e1c8ae33aa2d6af686e28a66cad3bc7e
kasia.kordys <kasia.kordys@gmail.com>
2026-01-07 22:39:07 +0200

    add sidebar and navbar with bootstrap

--- angular-dashboard/src/app/app.component.ts
+++ angular-dashboard/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, SidebarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
