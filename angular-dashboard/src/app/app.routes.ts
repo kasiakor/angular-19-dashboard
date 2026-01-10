64420bc27eef45d0840d927fd5e9a57b6e93cd7f
kasia.kordys <kasia.kordys@gmail.com>
2026-01-10 23:13:28 +0200

    add routing for charts and tables

--- angular-dashboard/src/app/app.routes.ts
+++ angular-dashboard/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
+import { ChartsComponent } from './charts/charts.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './table/table.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+  },
+  {
+    path: 'charts',
+    component: ChartsComponent,
+  },
+  {
+    path: 'tables',
+    component: TableComponent,
   },
 ];
